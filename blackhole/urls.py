b40de72af8cdd612690f1c1da411ffefb29b43f1
ikame <anler86@gmail.com>
2013-10-24 22:19:44 +0200

    Fixing django deprecation warning

--- blackhole/urls.py
+++ blackhole/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls.defaults import patterns, url
+# -*- coding: utf-8 -*-
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('blackhole.views',
